0206c8cf9bc94a0cf3390d0a40fa884763bde946
brianonzere <brianonzere18@gmail.com>
2023-07-27 09:26:03 +0300

    task 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,20 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i = 0;
+	int len = 0;
 
+	while ((dest[i]) != '\0')
+	{
+		len = len + 1;
+		i++;
+	}
+
+	for (i = 0; i < n && (src[i]) != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	return (dest);
 
 
 }
