5db62360dabcae52980a444d79fe6ce1dee51b10
brianonzere <brianonzere18@gmail.com>
2023-07-27 09:25:29 +0300

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int i = 0;
+	int len = 0;
 
+	while ((dest[i]) != '\0')
+	{
+		len = len + 1;
+		i++;
+	}
+
+	for (i = 0; (src[i]) != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	return (dest);
 
 
 }
