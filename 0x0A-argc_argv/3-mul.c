f22452eb8baa95561746cf975b00e95754715ec5
brianonzere <brianonzere18@gmail.com>
2023-08-04 22:56:05 +0300

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ * main - the start of the program
+ *
+ * @argc: numbe rof arguments
+ *
+ * @argv: the arguments passed
+ *
+ * Return: returns 0 when the program is successful
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int m = 1;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			m *= atoi(argv[i]);
+		}
+		printf("%d\n", m);
+	}
+	return (0);
+}
