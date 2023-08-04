363ca33a2a9e353a771bd06c82240155e00e2a49
brianonzere <brianonzere18@gmail.com>
2023-08-04 22:49:58 +0300

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - start of the program
+ *
+ * @argc: the number of argunments
+ *
+ * @argv: the arguments enterred
+ *
+ * Return: returns 0 to mean success
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int x;
+
+	if (argv[0])
+	{
+	x = argc - 1;
+
+	printf("%d\n", x);
+	}
+	return (0);
+}
