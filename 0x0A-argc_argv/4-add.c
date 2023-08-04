6ef5c922c050d7b6cd708787b01e4024d7595044
brianonzere <brianonzere18@gmail.com>
2023-08-04 22:56:58 +0300

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+/**
+ * main - the start of the program
+ *
+ * @argc: numbe rof arguments
+ *
+ * @argv: the arguments passed
+ *
+ * Return: returns 0 when the program is successful
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int m = 0;
+
+	if (argc ==  1)
+	{
+		m = 0;
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) > 0)
+			{
+			m += atoi(argv[i]);
+			}
+			else
+			{
+			printf("Error\n");
+			return (1);
+			}
+		}
+	}
+		printf("%d\n", m);
+		return (0);
+}
