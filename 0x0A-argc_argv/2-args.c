fb0012304ef5ddc8b4de57d77f0ae98c440527b4
brianonzere <brianonzere18@gmail.com>
2023-08-04 22:50:45 +0300

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - the start of the program
+ *
+ * @argc: number of arguments passed
+ *
+ * @argv: a poiter to the arguments passed
+ *
+ * Return: returns 0 when successful
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
