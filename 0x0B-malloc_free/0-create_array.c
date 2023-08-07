d64b1bbebfc955d56f802180eca88432ce7543aa
brianonzere <brianonzere18@gmail.com>
2023-08-07 20:59:57 +0300

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,8 +7,25 @@
  */
 char *create_array(unsigned int size, char c)
 {
+unsigned int i;
+char *m;
 
+if (size == 0)
+{
+	return (NULL);
+}
+m = malloc( size * sizeof(char));
 
+if (m == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i < size; i++)
+{
 
+	m[i] = c;
 
+}
+
+return (m);
 }
