b812030053595fc64830744719ef3353af91a4a7
brianonzere <brianonzere18@gmail.com>
2023-08-07 21:49:22 +0300

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,27 @@
 #include "main.h"
+
+/**
+ * st_len - strlen function
+ * @n: str piointer
+ * Return: int
+ */
+unsigned int st_len(char *n)
+{
+unsigned int b, lenm = 0;
+
+if (n == NULL)
+	return (0);
+for (b = 0; n[b] != '\0'; b++)
+{
+
+	lenm++;
+
+}
+return (lenm);
+}
+
+
+
 /**
  * str_concat - str_concat
  * @s1: s1 pointer
@@ -7,8 +30,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+unsigned int i, j, k;
+char *m;
+
+if (s1 == NULL)
+{
+	if (s2 == NULL)
+	{
+		return (NULL);
+	}
+}
+
+i = st_len(s1);
+j = st_len(s2);
 
 
+j = j + i + 1;
 
+m = malloc(j *sizeof(char));
 
+if (m == NULL)
+{
+	return (NULL);
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+	m[i] = s1[i];
+}
+for (k = 0; i < j; i++, k++)
+{
+	m[i] = s2[k];
+}
+return (m);
 }
