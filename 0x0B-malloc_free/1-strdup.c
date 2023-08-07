c8c9d930ec6c8a7ebfb565fe835ddb32b04981c6
brianonzere <brianonzere18@gmail.com>
2023-08-07 21:19:29 +0300

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,7 +6,24 @@
  */
 char *_strdup(char *str)
 {
+char *m;
+int i;
 
+if (str == NULL)
+	return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+	i++;
+}
 
+m = malloc(i * sizeof(char));
+
+if (m == NULL)
+	return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+{
+	m[i] = str[i];
+}
+return (m);
 
 }
