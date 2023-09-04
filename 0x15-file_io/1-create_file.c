98fd45ce571f5972a64a379598ad426d5d5832ec
onzerebrian <onzerebrian@gmail.com>
2023-09-04 20:35:13 +0300

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
-
 /**
- * append_text_to_file - appends text at the end of a file
+ * create_file - create a file named filename
  * @filename: name of the file
- * @text_content the NULL terminated string to add at the end of the file
- * Return: 1 on success and -1 on failure
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	register int _file, _write, text_len = 0;
 
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	_file = open(filename, O_WRONLY, O_APPEND);
+	_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (_file == -1)
 	{
 		return (-1);
